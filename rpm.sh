50f85b40dbeacee8fdd3f2cc3e92cd4a5f07077d
david <david@openlava.net>
2011-09-14 20:41:02 -0400

    Modified the rpmscript to install on our old build machine CentOs 4.6.

--- rpm.sh
+++ rpm.sh
@@ -8,6 +8,27 @@ set -x
 major="1"
 minor="0"
 
+grep 4.6 /etc/redhat-release > /dev/null
+if [ "$?" == "0" ]; then
+   echo "Cleaning..."
+   rm -f /usr/src/redhat/SOURCES/openlava*
+   rm -f /usr/src/redhat/RPMS/x86_64/openlava*
+   rm -f /usr/src/redhat/SPECS/openlava.spec
+
+   echo "Archiving..."
+   git archive --format=tar --prefix="openlava-${major}.${minor}/" HEAD \
+   | gzip > /usr/src/redhat/SOURCES/openlava-${major}.${minor}.tar.gz
+   cp spec/openlava.spec /usr/src/redhat/SPECS/openlava.spec
+
+  echo "RPM building..."
+  rpmbuild -ba --target x86_64 /usr/src/redhat/SPECS/openlava.spec
+  if [ "$?" != 0 ]; then
+    echo "Failed buidling rpm"
+    exit 1
+  fi
+  exit 0
+fi 
+   
 echo "Cleaning up ~/rpmbuild directory..."
 rm -rf ~/rpmbuild
 
