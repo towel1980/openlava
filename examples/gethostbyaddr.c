e008775b634e22da8db356cc2aed97485539fcd5
David Bigagli <david@openlava.net>
2011-11-07 18:50:15 +0100

    Add an example directory where to collect small test/example programs. This is good for support, study, test and fun!

---
+++ examples/gethostbyaddr.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+struct hostent *hp;
+struct in_addr a;
+
+int
+main(int argc, char **argv)
+{
+    in_addr_t addr;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s hostname\n", argv[0]);
+        return 1;
+    }
+
+    addr = inet_addr(argv[1]);
+    hp = gethostbyaddr(&addr, sizeof(in_addr_t), AF_INET);
+    if (hp) {
+
+        printf("name: %s\n", hp->h_name);
+        while (*hp->h_aliases)
+            printf("alias: %s\n", *hp->h_aliases++);
+
+        while (*hp->h_addr_list) {
+            char * p;
+            memcpy(&a, *hp->h_addr_list++, sizeof(a));
+            p = inet_ntoa(a);
+            printf("address: %s\n", inet_ntoa(a));
+        }
+    }
+
+    return 0;
+}
